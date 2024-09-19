635d73d5945f4864ac43bef66ae3a01daf8200b7
Aradhya <aradhya954@gmail.com>
2024-09-19 19:41:01 +0530

    first commit

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,21 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Craving Something Delicious? We've Got You Covered!</h2>
+      <p>
+      Pick from our mouth-watering meals and enjoy a feast, whether it's lunch or dinner.
+      </p>
+      <p>
+      Our chefs use fresh, premium ingredients to deliver great taste—satisfy your hunger at home.
+
+
+
+
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
